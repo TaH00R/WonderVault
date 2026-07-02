2282461572dfc3f45b496546cc786a6e57ad2abb
TaH00R <bgmiseason002@gmail.com>
2026-07-03 01:10:44 +0530

    Created Map Layout

--- lib/pages/map_screen.dart
+++ lib/pages/map_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
@@ -9,7 +12,43 @@ class MapScreen extends StatefulWidget {
 
 class _MapScreenState extends State<MapScreen> {
   @override
-  Widget build(BuildContext context) {  
-    return const Placeholder();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: FlutterMap(
+        options: const MapOptions(
+          cameraConstraint: const CameraConstraint.containLatitude(),
+          keepAlive: true,
+          backgroundColor: Colors.white,
+          initialCenter: LatLng(51.509364, -0.128928),
+          initialZoom: 13.0,
+          minZoom: 3.0,
+          maxZoom: 18.0,
+          interactionOptions: InteractionOptions(
+            flags:
+                InteractiveFlag.drag |
+                InteractiveFlag.pinchZoom |
+                InteractiveFlag.doubleTapZoom,
+          ),
+        ),
+        mapController: MapController(),
+        children: [
+          TileLayer(
+            urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+            userAgentPackageName: 'com.example.wondervault',
+            tileProvider: NetworkTileProvider(),
+            tileBuilder: darkModeTileBuilder,
+          ),
+          RichAttributionWidget(
+            attributions: [
+              TextSourceAttribution(
+                'OpenStreetMap contributors',
+                onTap: () =>
+                    launchUrl(Uri.parse('https://openstreetmap.org/copyright')),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
